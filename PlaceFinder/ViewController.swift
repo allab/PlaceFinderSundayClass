3c6404543fec79fe816be6775a06a8be520f09fe
frau <allab@ca.ibm.com>
2017-03-11 11:51:26 -0400

    Initial Commit

---
+++ PlaceFinder/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  PlaceFinder
+//
+//  Created by Alla Bondarenko on 2017-03-11.
+//  Copyright © 2017 SMU Student. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
